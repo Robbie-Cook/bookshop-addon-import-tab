9173634d437692d6de240c5b27b9e024e47a9a0f
Robbie Cook <robbieck7@gmail.com>
2021-01-11 19:37:04 +1300

    feat: moving logic up a component

--- addon/src/Panel.js
+++ addon/src/Panel.js
@@ -1,85 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import {
-  useChannel,
-  useParameter,
-  useStorybookState,
-  useStorybookApi,
-} from '@storybook/api';
 
 import SyntaxHighlighter from './SyntaxHighlighter';
 import style from 'react-syntax-highlighter/dist/esm/styles/hljs/github-gist';
-import { format as prettierFormat } from 'prettier/standalone';
-import prettier from 'prettier';
-import parser from 'prettier/parser-babel';
 
-/**
- * Go through an object tree and convert repeat-args to an array
- * @param {Record<string, any> | undefined} item
- */
-function searchTree(item) {
-  let newObject = { ...item };
-  if (item) {
-    const keys = Object.keys(item);
-    if (keys.length > 0) {
-      if (keys.includes('repeat-count')) {
-        newObject = [];
-        // Put the object in the array `repeat-count` times
-        const count = item['repeat-count'];
-        for (let i = 0; i < count; i++) {
-          delete item['repeat-count'];
-          newObject.push(item);
-        }
-      }
-
-      for (const key of keys) {
-        if (typeof newObject[key] === 'object') {
-          newObject[key] = searchTree(newObject[key]);
-        }
-      }
-    }
-  }
-  return newObject;
-}
-
-const HTMLPanel = () => {
-  const state = useStorybookState();
-  // const api = useStorybookApi();
-
-  const id = state.storyId;
-
-  let importText = '';
-
-  const story = state.storiesHash[id];
-  if (story) {
-    const componentParts = story.kind.split('/');
-    const componentName = componentParts[componentParts.length - 1];
-
-    // Turn args to a proper object
-    const normalizedArgs = {};
-    const entries = Object.entries(story.args);
-    for (const [key, item] of entries) {
-      // Don't include bookshop-reserved keys
-      if (key !== 'framework') {
-        if (key.includes('&&')) {
-          const items = key.split('&&');
-          normalizedArgs[items[0]] = {
-            ...normalizedArgs[items[0]],
-            [items[1]]: item,
-          };
-        } else {
-          normalizedArgs[key] = item;
-        }
-      }
-    }
-
-    // Handle repeat-count in storybook
-    const repeatedArgs = searchTree(normalizedArgs);
-
-    importText = `component("${componentName.toLowerCase()}", ${JSON.stringify(
-      repeatedArgs,
-    )})`;
-  }
-
+const HTMLPanel = (props) => {
   return (
     <SyntaxHighlighter
       language={'js'}
@@ -89,9 +13,7 @@ const HTMLPanel = () => {
       showLineNumbers={true}
       wrapLines={false}
     >
-      {`<%- ${prettier
-        .format(importText, { semi: false, parser: 'babel', plugins: [parser] })
-        .replace(/[\r\n]+$/, '')} %>`}
+      {`<%- ${props.code} %>`}
     </SyntaxHighlighter>
   );
 };
--- addon/src/register.js
+++ addon/src/register.js
@@ -1,20 +1,109 @@
 import React from 'react';
 import { AddonPanel } from '@storybook/components';
 import { addons, types } from '@storybook/addons';
+import prettier from 'prettier';
+import { format as prettierFormat } from 'prettier/standalone';
+import parser from 'prettier/parser-babel';
 
 import Panel from './Panel';
 
 const ADDON_ID = 'Bookshop';
 const PANEL_ID = `${ADDON_ID}/panel`;
 
+import {
+  useChannel,
+  useParameter,
+  useStorybookState,
+  useStorybookApi,
+} from '@storybook/api';
+
+/**
+ * Go through an object tree and convert repeat-args to an array
+ * @param {Record<string, any> | undefined} item
+ */
+function searchTree(item) {
+  let newObject = { ...item };
+  if (item) {
+    const keys = Object.keys(item);
+    if (keys.length > 0) {
+      if (keys.includes('repeat-count')) {
+        newObject = [];
+        // Put the object in the array `repeat-count` times
+        const count = item['repeat-count'];
+        for (let i = 0; i < count; i++) {
+          delete item['repeat-count'];
+          newObject.push(item);
+        }
+      }
+
+      for (const key of keys) {
+        if (typeof newObject[key] === 'object') {
+          newObject[key] = searchTree(newObject[key]);
+        }
+      }
+    }
+  }
+  return newObject;
+}
+
+/**
+ * A wrapper for the
+ */
+const LogicWrapper = (props) => {
+  const state = useStorybookState();
+
+  const id = state.storyId;
+
+  let text = '';
+
+  const story = state.storiesHash[id];
+  if (story) {
+    const componentParts = story.kind.split('/');
+    const componentName = componentParts[componentParts.length - 1];
+
+    // Turn args to a proper object
+    const normalizedArgs = {};
+    const entries = Object.entries(story.args);
+    for (const [key, item] of entries) {
+      // Don't include bookshop-reserved keys
+      if (key !== 'framework') {
+        if (key.includes('&&')) {
+          const items = key.split('&&');
+          normalizedArgs[items[0]] = {
+            ...normalizedArgs[items[0]],
+            [items[1]]: item,
+          };
+        } else {
+          normalizedArgs[key] = item;
+        }
+      }
+    }
+
+    // Handle repeat-count in storybook
+    const repeatedArgs = searchTree(normalizedArgs);
+
+    const importText = `component("${componentName.toLowerCase()}", ${JSON.stringify(
+      repeatedArgs,
+    )})`;
+
+    text = prettier
+      .format(importText, { semi: false, parser: 'babel', plugins: [parser] })
+      .replace(/[\r\n]+$/, '');
+  }
+
+  return (
+    <div>
+      <AddonPanel active={props.active} key={props.key}>
+        <Panel code={text} />
+      </AddonPanel>
+    </div>
+  );
+};
+
 addons.register(ADDON_ID, () => {
   addons.add(PANEL_ID, {
     title: 'Bookshop',
     type: types.PANEL,
-    render: ({ active, key }) => (
-      <AddonPanel active={active} key={key}>
-        <Panel />
-      </AddonPanel>
-    ),
+    render: ({ active, key }) => <LogicWrapper active={active} key={key} />,
   });
 });
